47e8b6b3dc91219d1b35ac719aa74441611ab3a4
juniorjmenezes <juniorjmenezes@gmail.com>
2023-08-02 09:43:44 -0300

    0943_020823

--- mail_client.php
+++ mail_client.php
@@ -4,14 +4,36 @@ require 'vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-function enviarMensagemEmail($checkin, $checkout, $nights, $guests, $name, $email, $phone) {
-    // Configurações do servidor SMTP
-    $smtp_server = 'smtp.kinghost.net';
+// Variável global para a conexão persistente SMTP
+$mail = null;
 
-    $port = 587;
-    $username = 'nao_responder@obahvillage.com.br';
-    $password = 'Obah@0307';
+// Função para obter uma conexão persistente SMTP
+function getMailConnection()
+{
+    global $mail;
 
+    if ($mail === null) {
+        $smtp_server = 'smtp.kinghost.net';
+        $port = 587;
+        $username = 'nao_responder@obahvillage.com.br';
+        $password = 'Obah@0307';
+
+        // Inicialização do PHPMailer
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = $smtp_server;
+        $mail->Port = $port;
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = 'tls';
+        $mail->Username = $username;
+        $mail->Password = $password;
+    }
+
+    return $mail;
+}
+
+function enviarMensagemEmail($checkin, $checkout, $nights, $guests, $name, $email, $phone)
+{
     // Limpa Telefone do Cliente
     $cleaned_phone_number = preg_replace("/[\(\)\-\s]/", "", $phone);
     $contact_phone_number = $cleaned_phone_number;
@@ -22,10 +44,18 @@ function enviarMensagemEmail($checkin, $checkout, $nights, $guests, $name, $emai
     $subject = 'Pedido de Reserva';
     $message = "
         <html>
-        <body style='padding: 20px'>
-            <p style='color: #2C3F58'>Olá $name,</p>
-            <p style='color: #2C3F58; font-weight: bold'>Já recebemos o seu pedido de reserva para $nights diária(s) em nossa Pousada. Seguem abaixo os detalhes:</p>
-            <ul style='color: #2C3F58'>
+        <head>
+            <style>
+                .p-30 { padding: 30px }
+                .text-obah { color: #2c3f58 }
+                .text-danger { color: red }
+                .fw-bold { font-weight: bold }
+            </style>
+        </head>
+        <body class='p-30'>
+            <p class='text-obah'>Olá $name,</p>
+            <p class='text-obah fw-bold'>Já recebemos o seu pedido de reserva para $nights diária(s) em nossa Pousada. Seguem abaixo os detalhes:</p>
+            <ul class='text-obah'>
                 <li>Check-in: $checkin</li>
                 <li>Check-out: $checkout</li>
                 <li>Hóspedes: $guests</li>
@@ -33,27 +63,15 @@ function enviarMensagemEmail($checkin, $checkout, $nights, $guests, $name, $emai
                 <li>Telefone: $contact_phone_number</li>
                 <li>E-mail: $email</li>
             </ul>
-            <p style='color: #2C3F58; font-weight: bold'>Aguarde nosso contato para confirmar o seu pedido, ou se preferir, fale diretamente conosco através do telefone/WhatsApp: +55 88 99664-2583.</p>
-            <p><small style='color: #2C3F58'><span style='color: red'>*</span> Lembramos que até o momento não foi efetuado nenhum bloqueio de disponibilidade de apartamento ou tarifa e os mesmos estão sujeitos à alteração sem aviso prévio. <br><span style='color: red'>**</span> Não é necessário responder a este email.</small></p>
+            <p class='text-obah fw-bold'>Aguarde nosso contato para confirmar o seu pedido, ou se preferir, fale diretamente conosco através do telefone/WhatsApp: +55 88 99664-2583.</p>
+            <p><small class='text-obah'><span class='text-danger'>*</span> Lembramos que até o momento não foi efetuado nenhum bloqueio de disponibilidade de apartamento ou tarifa e os mesmos estão sujeitos à alteração sem aviso prévio. <br><span class='text-danger'>**</span> Não é necessário responder a este email.</small></p>
             <img src='https://www.obahvillage.com.br/mail/logo_mail.png' alt='Obah Village'>
         </body>
         </html>
     ";
 
-    // Inicialização do PHPMailer
-    $mail = new PHPMailer();
-
-    // Configuração do servidor SMTP
-    $mail->isSMTP();
-    $mail->Host = $smtp_server;
-    $mail->Port = $port;
-    $mail->SMTPAuth = true;
-    $mail->SMTPSecure = 'tls';
-    $mail->Username = $username;
-    $mail->Password = $password;
-    //$mail->Sender = $from_email;
-
-    //$mail->SMTPDebug = 3;
+    // Inicialização do PHPMailer e uso da conexão persistente
+    $mail = getMailConnection();
 
     try {
         // Configurações do e-mail
