0ce7af7e42e44a5750a5b395d51216d14bac55d1
juniorjmenezes <juniorjmenezes@gmail.com>
2023-08-17 09:09:48 -0300

    0909_170823

--- partials/contacts/contacts.php
+++ partials/contacts/contacts.php
@@ -5,15 +5,19 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-12 col-md-6 col-lg-4 col-contact js-scroll-show">
+        <div class="col-12 col-md-6 col-lg-6 col-contact js-scroll-show">
             <h4 class="title title--h4">Endereço</h4>
             <p class="paragraph">Vila Preá, SN<br>Preá - Ceará - Brasil</p>
         </div>
-        <div class="col-12 col-md-6 col-lg-4 col-contact js-scroll-show">
+        <div class="col-12 col-md-6 col-lg-6 col-contact js-scroll-show">
             <h4 class="title title--h4">Reservas</h4>
             <p class="paragraph">reservas@obahvillage.com.br<br>+55 (88) 9 9664-2583</p>
         </div>
-        <div class="col-12 col-md-6 col-lg-4 col-contact js-scroll-show">
+        <div class="col-12 col-md-6 col-lg-6 col-contact js-scroll-show">
+            <h4 class="title title--h4">Agências/Operadoras de Turismo</h4>
+            <p class="paragraph">comercial@obahvillage.com.br<br>+55 (88) 9 9742-2584</p>
+        </div>
+        <div class="col-12 col-md-6 col-lg-6 col-contact js-scroll-show">
             <h4 class="title title--h4">Siga-nos</h4>
             <p class="paragraph">Conecte-se conosco no <a class="link-underline" href="https://instagram.com/pousadaobahvillage_?igshid=MzRlODBiNWFlZA==">Instagram.</a></p>
         </div>
